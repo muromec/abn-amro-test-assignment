ec2bd2e240a2d6fb4fbfea9978e2954742df31fc
Illya Petrov <ilya.muromec@gmail.com>
2023-09-11 21:39:19 +0200

    cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 import SearchForm from './components/SearchForm.vue'
 </script>
 
