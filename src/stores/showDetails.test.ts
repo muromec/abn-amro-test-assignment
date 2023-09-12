5ef692bfb16dcbe8e41a812f5ad988fcf0d4f9dd
Illya Petrov <ilya.muromec@gmail.com>
2023-09-12 18:38:12 +0200

    more coverage for data layer

---
+++ src/stores/showDetails.test.ts
@@ -0,0 +1,108 @@
+import { vi, expect, it, beforeEach, afterEach } from 'vitest'
+import { useShowsStore } from './shows'
+import { useShowDetails } from './showDetails'
+
+import { setActivePinia, createPinia } from 'pinia'
+
+import { MOCK_SHOW_LIST } from '@/mocks/shows'
+
+function makeHttpMock<ResponseType>(isOk: boolean, jsonData: ResponseType) {
+  const response = {
+    ok: isOk,
+    json: () => Promise.resolve(jsonData)
+  }
+  return async () => response
+}
+
+const mockedFetch = vi.fn()
+beforeEach(() => {
+  setActivePinia(createPinia())
+  vi.stubGlobal('fetch', mockedFetch.mockReset())
+})
+
+afterEach(() => {
+  vi.restoreAllMocks()
+})
+
+it('should not call API when created', () => {
+  const details = useShowDetails()
+  expect(mockedFetch).not.toBeCalled()
+})
+
+it('should load single show details)', async () => {
+  mockedFetch.mockImplementationOnce(makeHttpMock(true, []))
+  const details = useShowDetails()
+  await details.lazyLoadById(1)
+  expect(mockedFetch).toBeCalledWith('https://api.tvmaze.com/shows/1')
+  expect(mockedFetch).toHaveBeenCalledTimes(1)
+  expect(details.isErrorById(1).value).toBe(false)
+})
+
+it('should load details only once for the same id', async () => {
+  mockedFetch.mockImplementationOnce(makeHttpMock(true, []))
+  const details = useShowDetails()
+  await details.lazyLoadById(1)
+  expect(mockedFetch).toHaveBeenCalledTimes(1)
+  await details.lazyLoadById(1)
+  expect(mockedFetch).toHaveBeenCalledTimes(1)
+})
+
+it('should reuse entry details from cahce if it is present there', async () => {
+  mockedFetch.mockImplementationOnce(makeHttpMock(true, MOCK_SHOW_LIST))
+  const shows = useShowsStore()
+  await shows.load()
+  const details = useShowDetails()
+  await details.lazyLoadById(1)
+  expect(mockedFetch).toHaveBeenCalledTimes(1)
+  expect(mockedFetch).toBeCalledWith('https://api.tvmaze.com/shows')
+  await details.lazyLoadById(1)
+  expect(mockedFetch).toHaveBeenCalledTimes(1)
+})
+
+it('should load details separately for two different ids and keep both in cache', async () => {
+  mockedFetch
+    .mockImplementationOnce(makeHttpMock(true, MOCK_SHOW_LIST[1]))
+    .mockImplementationOnce(makeHttpMock(true, MOCK_SHOW_LIST[2]))
+
+  const details = useShowDetails()
+  await details.lazyLoadById(1)
+  expect(mockedFetch).toHaveBeenCalledTimes(1)
+  expect(mockedFetch).toBeCalledWith('https://api.tvmaze.com/shows/1')
+
+  await details.lazyLoadById(2)
+  expect(mockedFetch).toHaveBeenCalledTimes(2)
+  expect(mockedFetch).toBeCalledWith('https://api.tvmaze.com/shows/2')
+
+  expect(details.findDetails(1).value).toEqual(MOCK_SHOW_LIST[1])
+  expect(details.findDetails(2).value).toEqual(MOCK_SHOW_LIST[2])
+})
+
+it('should expose error flag and return details as null if request fails', async () => {
+  mockedFetch.mockImplementationOnce(makeHttpMock(false, []))
+  const details = useShowDetails()
+  expect(details.isErrorById(1).value).toBe(false)
+
+  await details.lazyLoadById(1)
+  expect(details.findDetails(1).value).toEqual(null)
+  expect(details.isErrorById(1).value).toBe(true)
+})
+
+it('should expose loading and error flags per entry id', async () => {
+  mockedFetch.mockImplementationOnce(makeHttpMock(false, []))
+  const details = useShowDetails()
+  // before
+  expect(details.isErrorById(1).value).toBe(false)
+  expect(details.isLoadingById(1).value).toBe(false)
+  const loadingTask = details.lazyLoadById(1)
+  // during
+  expect(details.isErrorById(1).value).toBe(false)
+  expect(details.isLoadingById(1).value).toBe(true)
+  expect(details.isLoadingById(2).value).toBe(false)
+
+  await loadingTask
+  // after
+  expect(details.isErrorById(1).value).toBe(true)
+  expect(details.isErrorById(2).value).toBe(false)
+
+  expect(details.isLoadingById(1).value).toBe(false)
+})
---
+++ src/stores/showsSearch.test.ts
@@ -0,0 +1,85 @@
+import { vi, expect, it, beforeEach, afterEach } from 'vitest'
+import { useShowsSearchStore } from './showsSearch'
+
+import { setActivePinia, createPinia } from 'pinia'
+
+import { MOCK_SHOW_LIST } from '@/mocks/shows'
+
+function makeHttpMock<ResponseType>(isOk: boolean, jsonData: ResponseType) {
+  const response = {
+    ok: isOk,
+    json: () => Promise.resolve(jsonData)
+  }
+  return async () => response
+}
+
+const mockedFetch = vi.fn()
+beforeEach(() => {
+  setActivePinia(createPinia())
+  vi.stubGlobal('fetch', mockedFetch.mockReset())
+})
+
+afterEach(() => {
+  vi.restoreAllMocks()
+})
+
+it('should have no list in initial state', () => {
+  const search = useShowsSearchStore()
+  expect(search.list).toBe(null)
+})
+
+it('should not call API when created', () => {
+  const search = useShowsSearchStore()
+  expect(mockedFetch).not.toBeCalled()
+})
+
+it('should pass search term to ?q= of /shows endpoint', async () => {
+  mockedFetch.mockImplementationOnce(makeHttpMock(true, []))
+  const search = useShowsSearchStore()
+  await search.searchFor('Drama')
+  expect(mockedFetch).toBeCalledWith(new URL('https://api.tvmaze.com/search/shows?q=Drama'))
+  expect(mockedFetch).toHaveBeenCalledTimes(1)
+  expect(search.list).toEqual([])
+  expect(search.isError).toBe(false)
+})
+
+it('should keep cached search results if the terms did not change', async () => {
+  mockedFetch.mockImplementationOnce(makeHttpMock(true, []))
+  const search = useShowsSearchStore()
+  await search.searchFor('Drama')
+  expect(mockedFetch).toHaveBeenCalledTimes(1)
+  await search.searchFor('Drama')
+  expect(mockedFetch).toHaveBeenCalledTimes(1)
+})
+
+it('should reload search results if search term changed', async () => {
+  mockedFetch.mockImplementationOnce(makeHttpMock(true, []))
+  const search = useShowsSearchStore()
+  await search.searchFor('Drama')
+  expect(mockedFetch).toHaveBeenCalledTimes(1)
+  await search.searchFor('Mystery')
+  expect(mockedFetch).toHaveBeenCalledTimes(2)
+})
+
+it('should keep list set to null if loading errors out', async () => {
+  mockedFetch.mockImplementationOnce(makeHttpMock(false, []))
+  const search = useShowsSearchStore()
+  await search.searchFor('Drama')
+  expect(search.list).toEqual(null)
+})
+
+it('should expose loading and error flags', async () => {
+  mockedFetch.mockImplementationOnce(makeHttpMock(false, []))
+  const search = useShowsSearchStore()
+  // before
+  expect(search.isError).toBe(false)
+  expect(search.isLoading).toBe(false)
+  const loadingTask = search.searchFor('Drama')
+  // during
+  expect(search.isError).toBe(false)
+  expect(search.isLoading).toBe(true)
+  await loadingTask
+  // after
+  expect(search.isError).toBe(true)
+  expect(search.isLoading).toBe(false)
+})
